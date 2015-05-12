c01417a77568c6459d5279c386fc49e61054a06f
Alex Edelstein <alex@edelstein.org>
2015-05-12 13:40:38 -0700

    initial commit

---
+++ DocusignTutorial/Controllers/SignatureController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Ajax.Utilities;
+using RestSharp;
+
+namespace DocusignTutorial.Controllers
+{
+    public class SignatureController : Controller
+    {
+
+        string baseurl = "https://demo.docusign.net/restapi/v2/";
+
+        // GET: Signature
+        public ActionResult Index()
+        {
+            ShowLoginInfo();
+            RequestSignature();
+
+            return View();
+
+        }
+
+        public ActionResult ShowLoginInfo()
+        {
+            string header_authstring =
+               @"{""Username"": ""9b322d6f-e6d0-49b0-b609-5c896318d366"", ""Password"": ""foobarhead1"",""IntegratorKey"": ""DOCU-af51f9bf-1b53-4ca7-b5e2-24adcbb5e6f9""}";
+
+            var client = new RestClient();
+            client.BaseUrl = new Uri(baseurl + "login_information");
+
+            var request = new RestRequest();
+            request.AddHeader("Content-Type", "application/json");
+            request.AddHeader("Accept", "application/json");
+            request.AddHeader("X-DocuSign-Authentication", header_authstring);
+
+            IRestResponse response = client.Execute(request);
+
+          
+
+            return View(response);
+        }
+
+        public ActionResult RequestSignature()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ DocusignTutorial/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DocusignTutorial.Startup))]
+namespace DocusignTutorial
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
